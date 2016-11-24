d422df7983975eb050c748cff3deb91699c34724
Cristian E. Avalos <cavalos0086@gmail.com>
2016-11-24 08:43:29 -0500

    introduces 2way data binding to hero input

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // allows 2way data binding for form inputs
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
