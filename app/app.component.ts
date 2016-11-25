c02a3832c83ff9cc0d2910cd96eb8acc673b29e4
Cristian E. Avalos <cavalos0086@gmail.com>
2016-11-25 00:03:24 -0500

    moves html out of component.ts into its own file

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/heroes">Heroes</a>
-      <a routerLink="/dashboard">Dashboard</a>
-      <router-outlet></router-outlet>
-    </nav>
-  `,
+  templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 
