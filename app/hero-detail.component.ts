36a0ff9c7c6ff0ca1314f2925432d9befc5ba1da
Cristian E. Avalos <cavalos0086@gmail.com>
2016-11-24 23:12:08 -0500

    creates a selected hero view and changes hero-detail component to reflect that

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-hero-detail',
@@ -15,7 +21,20 @@ import { Hero } from './hero';
   `
 })
 
-export class HeroDetailComponent {
-  @Input()
+export class HeroDetailComponent implements OnInit {
   hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    // the hero id is a number and the params is always a string. therefore the addition of +
+    this.route.params.switchMap((params: Params) =>
+      this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
 }
