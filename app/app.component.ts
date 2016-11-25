57d5e8cb7f8946534e3b6f3c5a9a7382e154b0f1
Cristian E. Avalos <cavalos0086@gmail.com>
2016-11-24 22:24:41 -0500

    adds routing to the heroes page

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,11 @@ import { HeroesComponent } from './heroes.component';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a routerLink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
   `
 })
 
-export class AppComponents {
+export class AppComponent {
   title = 'Tour of Heroes';
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // allows 2way data binding for form inputs
+import { RouterModule } from '@angular/router'; // needed for routing
 
 //////////////////////////////////
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
