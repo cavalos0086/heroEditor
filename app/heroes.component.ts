d26d55fdad800f6b083690a10ea39354292cbae8
Cristian E. Avalos <cavalos0086@gmail.com>
2016-11-24 23:53:01 -0500

    add routing to hero.component file on gotoDetail method

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -16,9 +17,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {
-
-  }
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -30,5 +32,9 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
